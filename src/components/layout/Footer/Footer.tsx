9b81f95fbd43a90ee2fc5958c5a3156f04fdc635
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-09-26 20:25:25 +0500

    Fixed image button footericon

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -67,9 +67,11 @@ const Footer = () => {
             Superball X Limited is a company registered in Scotland with the
             company number SC737654. Superball X Limited's trading names are
             superballx.co.uk and superballx.com. Contact us by post at 272 Bath
-            Street, Glasgow, G2 4JR, Scotland or by emailing
-            {" "}<a className="email" href="mailto:gavin@superballx.com">gavin@superballx.com.</a>
-            {" "}2022 © Superball X. All rigts reserved.{" "}
+            Street, Glasgow, G2 4JR, Scotland or by emailing{" "}
+            <a className="email" href="mailto:gavin@superballx.com">
+              gavin@superballx.com.
+            </a>{" "}
+            2022 © Superball X. All rigts reserved.{" "}
           </Typography>
         </Container>
       </Box>
